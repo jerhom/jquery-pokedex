cdf7f8cb3ef836bac3c2301035ebdf728efac5d4
jerhom <jhperado10@gmail.com>
2019-01-22 17:36:46 +0800

    adding files

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+$(function() {
+  var pokemonSearch;
+  var defaultPokemon = 1;
+  var defaultPokemonData;
+
+  var initFunc = function() {
+    defaultPokemonData = $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon/" + defaultPokemon,
+      method: "GET"
+    });
+
+    defaultPokemonData.done(function(data) {
+      defaultPokemonData = data;
+      $("loading-container").removeClass("active");
+      $(".pokedex h3").html(data.name.toUpperCase());
+      $(".poke-img img").attr("src", data.sprites.front_default);
+      console.log(data);
+    });
+
+    defaultPokemonData.fail(function(jqXHR, textStatus, error) {
+      alert("Request failed: " + textStatus + " " + error);
+    });
+  };
+
+  initFunc();
+
+  $(".btn").on("click", function() {
+    pokemonSearch = $('.pokedex input[type="text"]').val();
+    var request = $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch,
+      method: "GET"
+    });
+
+    request.done(function(data) {
+      $(".pokedex h3").html(data.name.toUpperCase());
+      $(".poke-img img").attr("src", data.sprites.front_default);
+      console.log(data);
+    });
+
+    request.fail(function(jqXHR, textStatus, error) {
+      alert("Request failed: " + textStatus + " " + error);
+    });
+  });
+});
